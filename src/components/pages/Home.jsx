0587067e13e99a9efd3b9a0dbbdca61679381aca
CharlesKibet <charlesbiegon973@gmail.com>
2024-06-10 18:21:34 +0300

    React router dom added

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,14 +2,17 @@ import Private from '../private/Private';
 import Public from '../public/Public';
 import Nav from '../nav/Nav';
 import './home.css';
+import Moving from '../move/Moving';
 
 function Home() {
   return (
     <>
+      <a>Home</a>
       <Nav />
       <div className="home">
         <Public />
         <Private />
+        <Moving />
       </div>
     </>
   );
--- src/components/public/Public.jsx
+++ src/components/public/Public.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './public.css';
-import Search from './Search';
+
 
 function Public() {
   const [display, setDisplay] = useState(false);
