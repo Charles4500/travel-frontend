17d220535a8269662673afeb09522b626f1842ae
CharlesKibet <charlesbiegon973@gmail.com>
2024-06-12 15:51:47 +0300

    Fect h data almost done

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,17 +1,34 @@
+import { useState } from 'react';
 import './search.css';
 
 function Search() {
+  const [formData, setFormData] = useState({
+    from: '',
+    to: '',
+    date: '',
+    passengers: '',
+  });
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+  function handleInput(event) {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+    console.log(value);
+  }
   return (
     <div className="form-one">
       <section className="add-card page">
-        <form className="form">
-          <label for="name" className="label">
+        <form className="form" onSubmit={handleSubmit}>
+          <label for="location" className="label">
             <span className="title">Enter your location</span>
             <input
               className="input-field"
               type="text"
-              name="input-location"
+              name="from"
               placeholder="Enter your location"
+              value={formData.from}
+              onChange={handleInput}
             />
           </label>
           <label for="serialCardNumber" class="label">
@@ -19,8 +36,10 @@ function Search() {
             <input
               className="input-field"
               type="text"
-              name="input-name"
+              name="to"
               placeholder="Anywhere"
+              value={formData.to}
+              onChange={handleInput}
             />
           </label>
           <div class="split">
@@ -29,8 +48,10 @@ function Search() {
               <input
                 className="input-field"
                 type="date"
-                name="input-date"
+                name="date"
                 placeholder="Date"
+                value={formData.date}
+                onChange={handleInput}
               />
             </label>
             <label for="cvv" className="label">
@@ -38,8 +59,10 @@ function Search() {
               <input
                 className="input-field"
                 type="number"
-                name="amount"
+                name="passengers"
                 placeholder="boarding.."
+                value={formData.passengers}
+                onChange={handleInput}
               />
             </label>
           </div>
