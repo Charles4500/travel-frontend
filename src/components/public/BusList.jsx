5f9a2045f41076cdcd3c044437b05af2b44d082a
CharlesKibet <charlesbiegon973@gmail.com>
2024-06-16 22:10:00 +0300

    Bus updates

--- src/components/public/BusList.jsx
+++ src/components/public/BusList.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 function BusList({ name, price, passengers, location_from, location_to }) {
   function handleClick() {
-    alert('Ticket has been added');
+    alert('Ticket Added successfully');
+  }
+  function removeTicket() {
+    alert('Ticket deleted successfully');
   }
   return (
     <div>
@@ -40,6 +43,7 @@ function BusList({ name, price, passengers, location_from, location_to }) {
             <button
               class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
               type="button"
+              onClick={removeTicket}
             >
               Cancel Ticket
             </button>
